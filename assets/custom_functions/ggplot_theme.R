87ec53d03dadf9c4527618da78429819b6b388f9
andreslagerlof <mail@andres.nu>
2024-03-21 22:37:16 +0100

    Reshuffle code and start a new post

---
+++ assets/custom_functions/ggplot_theme.R
@@ -0,0 +1,19 @@
+
+# Define function for ggplot theme ----------------------------------------
+
+#Define fff_theme() function
+
+theme_fff <- function(){ 
+  
+  theme_minimal() %+replace%    #replace elements we want to change
+    
+    theme(
+      panel.grid.major.x = element_blank(),
+      panel.grid.major.y = element_blank(),
+      plot.title = element_text(
+        size = rel(1.2)
+      ),
+      plot.title.position = "plot"
+    )
+}  
+
---
+++ assets/source_files/libraries_load.R
@@ -0,0 +1,15 @@
+
+# About -------------------------------------------------------------------
+
+# This file contains libraries to be used
+
+
+# Libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(scales)
+library(gghighlight)
+library(patchwork)
+library(RColorBrewer)
+library(epoxy)
+library(gt)
